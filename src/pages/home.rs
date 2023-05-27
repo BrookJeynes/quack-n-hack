8c5d68667b5e76e86613f26d10f5da16cc174de7
Rushi <44952533+Skyliegirl33@users.noreply.github.com>
2023-05-27 03:10:43 +0200

    confetti test pr (#8)
    * Even more confetti
    
    ---------
    
    Co-authored-by: Brook Jeynes [SSW] <jeynesbrook@gmail.com>

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -29,7 +29,7 @@ pub fn Home(cx: Scope) -> impl IntoView {
         complete();
         let _ = js_sys::eval(
             "window.confetti({
-                    particleCount: 200,
+                    particleCount: 650,
                     spread: 100,
                     origin: { y: 0.6 }
                 });",
